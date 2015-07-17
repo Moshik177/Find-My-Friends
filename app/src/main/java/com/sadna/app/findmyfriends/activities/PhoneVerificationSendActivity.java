e052653d80b131cdfbd4ecda90bb1303197e1bd2
Avihoo Mamka <avihoo.mamka@gmail.com>
2015-07-17 23:39:38 +0300

    removed unused imports

--- app/src/main/java/com/sadna/app/findmyfriends/activities/PhoneVerificationSendActivity.java
+++ app/src/main/java/com/sadna/app/findmyfriends/activities/PhoneVerificationSendActivity.java
@@ -1,6 +1,5 @@
 package com.sadna.app.findmyfriends.activities;
 
-import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
